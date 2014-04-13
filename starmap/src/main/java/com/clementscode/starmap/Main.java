2db59740551236dfcab56832cd8fb868695ec848
Greg Clements <gregnebriowa@gmail.com>
2014-04-12 20:32:16 -0500

    working standalone with jmap image. start of webapp

--- starmap/src/main/java/com/clementscode/starmap/Main.java
+++ starmap/src/main/java/com/clementscode/starmap/Main.java
@@ -1,8 +1,8 @@
 package com.clementscode.starmap;
 
-import java.io.FileReader;
+import java.util.Collection;
 
-import au.com.bytecode.opencsv.CSVReader;
+import com.clementscode.starmap.dao.StarDao;
 
 public class Main {
 
@@ -10,73 +10,31 @@ public class Main {
 		run();
 	}
 
-	private static class Stars {
-		String Id, HipNum, stringRA, stringDec, stringMag, ProperName;
-	}
-
 	public static void run() throws Exception {
-		// EntityManagerFactory emf = Persistence
-		// .createEntityManagerFactory("test");
-		// EntityManager em = emf.createEntityManager();
-		// EntityTransaction tx = em.getTransaction();
+		StarDao dao = new StarDao();
 
 		// the following from http://opencsv.sourceforge.net/
+		// StarData test = em.find(StarData.class, 1); // get item with id of 1
 
-		CSVReader reader = new CSVReader(new FileReader("stars.csv"), '\t');
-		String[] nextLine;
-		Stars stringStarData[];
-		stringStarData = new Stars[600];
-
-		int numStars = 0;
-
-		reader.readNext();
-
-		while ((nextLine = reader.readNext()) != null) {
-			// nextLine[] is an array of values from the line
-			// System.out.println(nextLine[0] + nextLine[1] + "etc...");
-
-			Stars current = new Stars();
-
-			// how do I set up array???
-			current.Id = nextLine[0]; // id number in file int
-			current.HipNum = nextLine[1]; // Hipparchos number
-											// int
-			current.ProperName = nextLine[2]; // ProperName
-			current.stringRA = nextLine[3]; // Right Ascension
-											// double
-			current.stringDec = nextLine[4]; // Declination
-												// double
-			current.stringMag = nextLine[5]; // Magnitude
-												// smaller is
-												// brighter
-												// double
-			stringStarData[numStars] = current;
-			numStars++;
+		try {
+			if (!dao.dbExists()) {
+				System.out.println("The StarData db already exists");
+				// this query sets the RA range, the Declination range, and the
+				// dimmest magnitude
+				Collection<StarData> data = dao.querydb(2.0, 9.0, -30.0, 30.0,
+						5.0);
+				System.out.println(data.size()
+						+ " records match the query in the database");
+				SpatialConverter.render(data);
+				// for (StarData data : all) {
+				// System.out.println(data);
+				// }
+			} else {
+				dao.populateDb();
+			}
+		} finally {
+			dao.close();
 		}
-
-		// show the first line of the database
-		System.out.println("1st record " + stringStarData[0].Id + " "
-				+ stringStarData[0].stringRA);
-		System.out.println("2nd record " + stringStarData[1].Id + " "
-				+ stringStarData[1].stringRA);
-		// em.close();
-		// emf.close();
 	}
-}
 
-/*
- * the following were in the orginal main.java file Test test =
- * em.find(Test.class, 1); // get item with id of 1 if (test == null) {
- * System.out.println("None found need to create"); test = new Test(); test.id =
- * 1; test.data = "a";
- * 
- * tx.begin(); em.persist(test); // store
- * 
- * tx.commit(); } else { System.out.println("Test found in database"); }
- * 
- * System.out.format("Test{id=%s, data=%s}\n", test.id, test.data);
- * 
- * em.close(); emf.close(); }
- * 
- * }
- */
+}
--- starmap/src/main/java/com/clementscode/starmap/StarData.java
+++ starmap/src/main/java/com/clementscode/starmap/StarData.java
@@ -5,23 +5,35 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Index;
+
 @Entity
+@org.hibernate.annotations.Table(appliesTo = "StarData", indexes = { @Index(name = "COORDIDX", columnNames = {
+		"RA", "Decln" }) })
 @Table(name = "StarData")
 public class StarData {
 
+	// TODO make vars lowercase start
 	@Id
 	// for stars.csv file column headers
-	public int Id; // number in file
+	public int id; // number in file
 
 	@Column
-	public int HipNum; // Hipparchos star catalog number
+	public int hipNum; // Hipparchos star catalog number
 	@Column
-	public String ProperName; // name of star, may be blank
+	public String properName; // name of star, may be blank
 	@Column
-	public double RA; // Right Ascension 0 to 24
+	public double ra; // Right Ascension 0 to 24
 	@Column
-	public double Dec; // Declination +90 to -90 degrees
+	public double decln; // Declination +90 to -90 degrees
 	@Column
-	public double Mag; // brightness more negative is brighter
+	public double mag; // brightness more negative is brighter
+
+	@Override
+	public String toString() {
+		return "StarData [Id=" + id + ", HipNum=" + hipNum + ", ProperName="
+				+ properName + ", RA=" + ra + ", Decln=" + decln + ", Mag="
+				+ mag + "]";
+	}
 
 }
